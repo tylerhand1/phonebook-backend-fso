15667a377c848a6fc7d5e9a7e890265a63b7f764
tylerhand1 <tyha7462@colorado.edu>
2024-02-01 23:57:39 -0700

    Exercise 3.4

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ app.get('/info', (req, res) => {
     `)
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if(person) {
+        persons = persons.filter(p => p.id !== id)
+        
+        res.status(204).end()
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
