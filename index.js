1c83c4945691a1a58d081789e05162ec8781a387
tylerhand1 <tyha7462@colorado.edu>
2024-02-01 23:51:59 -0700

    Exercise 3.3

--- index.js
+++ index.js
@@ -30,6 +30,16 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 app.get('/info', (req, res) => {
     const personCount = persons.length
 
