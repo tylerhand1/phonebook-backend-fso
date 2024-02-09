b7c7a5537632cc6aaef850f323199992c11a1591
tylerhand1 <tyha7462@colorado.edu>
2024-02-08 17:32:29 -0700

    Exercise 3.11

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 
 const cors = require('cors')
